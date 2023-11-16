930aa9f3365c869851a67da628c8d2bb41b0cb6d
WebGCDev <gaetano.chi@gmail.com>
2023-11-16 19:14:32 +0100

    feat: add Posizione Random Bombe whit ColorRED & Fix Function init

--- js/script.js
+++ js/script.js
@@ -1,23 +1,19 @@
 const container = document.querySelector('.container');
 const choiceLevel = document.querySelector('.choice-level');
 const chooseLevel = document.getElementById('stages');
+let numberBlackList = [];
+let counterPoints = 0;
+let endCondition = false;
 
 reset();
 
-function init(stage, n) {
+//Semplifico la funzione
+function init(stage, n){
   for (let i = 1; i <= n; i++) {
-    const square = genSquare(i);
-    square.classList.add(stage);
-
-    // Clicca sul quadrato e con metodo stampato in console ci permetterà di vedere quante volte clicchiamo su un determinato quadrato con relativo counter
-    square.addEventListener('click', function () {
-      console.log(this.id);
-      this.classList.toggle('checked');
-    });
-
-    container.append(square);
-  }
-}
+    let square = genSquare(getRandomNumber(1, n));
+   square.classList.add(stage)  
+      container.append(square);
+      }  
 
 //Bottore Start & Select Level
 function genBtnStart() {
@@ -43,7 +39,43 @@ function genSquare(index) {
   const newSquare = document.createElement('div');
   newSquare.className = 'square';
   newSquare.id = index;
+  newSquare.addEventListener('click',clickedCheck)
   return newSquare;
+}
+
+//Funzione che mi permetta di cliccare sul quadrato
+function clickedCheck(){
+  if(this.id <= 15 ){
+    this.classList.add('bomb')
+    console.log(this.id);
+    return lose;
+  }else{
+    this.classList.add('checked')
+    this.removeEventListener('click',clickedCheck)
+    counterPoints++;
+    console.log(counterPoints);
+    console.log(this.id);
+  }
+}
+
+
+// Funzione per generare un numero casuale univoco
+function uniqueRandomNumber(min, max) {
+  let randomNumber;
+  let controlInList = false;
+  while (!controlInList) {
+    randomNumber = getRandomNumber(min, max);
+    if (!numberBlackList.includes(randomNumber)) {
+      numberBlackList.push(randomNumber);
+      controlInList = true;
+    }
+  }
+  return randomNumber;
+}
+
+// Numeri random generati con funzione
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Reset della funzione
